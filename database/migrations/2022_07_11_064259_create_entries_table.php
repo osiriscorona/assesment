df2a184597fcdf83476d628d558a5f700e58bf85
Osiris Corona <osiris.corona92@gmail.com>
2022-07-11 13:17:07 -0500

    deleted unused column in entries table

--- database/migrations/2022_07_11_064259_create_entries_table.php
+++ database/migrations/2022_07_11_064259_create_entries_table.php
@@ -17,7 +17,6 @@ return new class extends Migration
             $table->id();
             $table->string('title');
             $table->unsignedBigInteger('parent_id')->nullable();
-            $table->unsignedInteger('max_childs')->default(0);
             $table->timestamps();
         });
     }
