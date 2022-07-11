6c41f0b673ac1e8cbdf74882d6cad4784c644c5c
Osiris Corona <osiris.corona92@gmail.com>
2022-07-11 13:18:29 -0500

    css fixes and add entires to navigation menu

--- resources/views/entries/childs.blade.php
+++ resources/views/entries/childs.blade.php
@@ -1,6 +1,6 @@
 <ul class='list-decimal'>
     @foreach($childs as $child)
-        <li>
+        <li class="pl-4">
             {{ $child->title }}
             @if(count($child->childs) > 0)
                 @include('entries.childs', ['childs' => $child->childs])
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -12,8 +12,8 @@
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
+                    <x-nav-link :href="route('entries.index')" :active="request()->routeIs('entries.index')">
+                        {{ __('Entries') }}
                     </x-nav-link>
                 </div>
             </div>
@@ -63,11 +63,8 @@
     <!-- Responsive Navigation Menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
-            <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                {{ __('Dashboarasdfasdfsdad') }}
-            </x-responsive-nav-link>
             <x-responsive-nav-link :href="route('entries.index')" :active="request()->routeIs('entries.index')">
-                {{ __('Hierarchy Tree') }}
+                {{ __('Entries') }}
             </x-responsive-nav-link>
         </div>
 
